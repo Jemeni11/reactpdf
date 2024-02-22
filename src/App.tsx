d8e3d17bf5a97d26cb230ce53de21c4ef31c91c5
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-02-22 14:03:35 +0100

    Add PDF rendering functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import { pdfjs, Document, Page } from "react-pdf";
+import type { PDFDocumentProxy } from "pdfjs-dist";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import { TextContent, TextItem } from "pdfjs-dist/types/src/display/api";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
+
+const options = {
+  cMapUrl: "/cmaps/",
+  standardFontDataUrl: "/standard_fonts/",
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [hasFileBeenSelected, setHasFileBeenSelected] = useState(false);
+  const [numPages, setNumPages] = useState<number>();
+  const [file, setFile] = useState<File | null>(null);
 
-  return (
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setHasFileBeenSelected(true);
+    setFile(event.target.files![0]);
+  }
+
+  function onDocumentLoadSuccess({
+    numPages: nextNumPages,
+  }: PDFDocumentProxy): void {
+    setNumPages(nextNumPages);
+  }
+
+  function formatText(texts: TextContent) {
+    let textFinal = "";
+    for (let i = 0; i < texts.items.length; i++) {
+      const item = texts.items[i] as TextItem;
+      textFinal += item.str;
+    }
+    console.log(textFinal);
+  }
+
+  return hasFileBeenSelected ? (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Document
+        file={file}
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={options}
+      >
+        {Array.from(new Array(numPages), (_, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+            renderAnnotationLayer={false}
+            renderTextLayer={true}
+            onGetTextSuccess={(text) => formatText(text)}
+            onGetTextError={(e) => console.log(e)}
+          />
+        ))}
+      </Document>
     </>
-  )
+  ) : (
+    <>
+      <label htmlFor="pdf_input">Select a pdf file</label>
+      <input
+        type="file"
+        id="pdf_input"
+        name="pdf_input"
+        accept=".pdf"
+        style={{ opacity: 0 }}
+        onChange={handleInputChange}
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
